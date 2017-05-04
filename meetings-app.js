63a8545c1de56b9c69c612ae0d7dcfda112e6349
Ryan Weddle <weddle@gmail.com>
2017-05-04 10:05:09 -0400

    added 2-step response with private message to meeting initiator

--- meetings-app.js
+++ meetings-app.js
@@ -9,8 +9,6 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.VERIFIC
 
 var webex_site = process.env.WEBEX_SITE;
 
-
-
 var options = {
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
@@ -34,31 +32,52 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function(slashcom, msg) {
   switch(msg.command) {
     case "/webex" : // handle the '/webex' slash command
-      slashcom.replyPublic(msg,
-        userLink(msg) + " has scheduled an instant WebEx meeting.\n"
-        + "<" + getPmrLink(msg.user_name, webex_site) +"|Click here to join with WebEx>");
+      slashcom.replyPrivate(msg,
+          "Click the link below to start your meeting:",
+          function () {
+            slashcom.replyPublicDelayed(msg,
+              userLink(msg) + " has scheduled an instant WebEx meeting.\n"
+              + "<" + getPmrLink(msg.user_name, webex_site) +"|Click here to join.>"
+            );
+          });
+
     break;
 
     case "/spark" : // handle the '/spark' slash command
-      slashcom.replyPublic(msg,
-        userLink(msg) + " has started a meeting.\n"
-        + "<" + getSparkLink(msg.user_name, webex_site) +"|Click here to join with Spark>");
+
+    slashcom.replyPrivate(msg,
+      "Click the link below to start your meeting:",
+        function () {
+          slashcom.replyPublicDelayed(msg,
+            userLink(msg) + " has started a meeting.\n"
+            + "<" + getSparkLink(msg.user_name, webex_site) +"|Click here to join with Spark>"
+          );
+        });
+
     break;
 
     case "/pmr" : // test case for richer '/pmr' slash command
-      slashcom.replyPublic(msg,
-      userLink(msg) + " has scheduled an instant WebEx meeting.\n"
-      + "<" + getPmrLink(msg.user_name, webex_site) +"|Click here to join with WebEx>\n"
-      + "<" + getSparkLink(msg.user_name, webex_site) +"|Click here to join with Spark>");
+
+    slashcom.replyPrivate(msg,
+      "Click one of the links below to start your meeting:",
+        function () {
+          slashcom.replyPublicDelayed(msg,
+            userLink(msg) + " has started a meeting.\n"
+            + "<" + getPmrLink(msg.user_name, webex_site) +"|Click here to join with WebEx>\n"
+            + "<" + getSparkLink(msg.user_name, webex_site) +"|Click here to join with Spark>"
+          );
+        });
+
     break;
 
     default:
-    slashcom.replyPublic(msg, "I'm sorry, I don't understand: " + msg.command);
+    slashcom.replyPrivate(msg, "I'm sorry, I don't understand: " + msg.command);
   }
 });
 
 
 // Helper Functions
+
 
 function userLink (msg) {
   return "<@" + msg.user + "|" + msg.user_name + ">";
